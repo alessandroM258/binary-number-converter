4da5c235826be8167125000484458e1137af4b48
alessandroM258 <144828778+alessandroM258@users.noreply.github.com>
2024-12-17 18:58:43 +0000

    convert binary to decimal

--- converter.py
+++ converter.py
@@ -1,1 +1,8 @@
-
+def decimalToBinary(num):
+    binary_string = bin(num)[2:]
+    print(binary_string)
+theSandwich = int(input("enter binary number and convert it to a decimal"))
+if theSandwich >= 0:
+    decimalToBinary(theSandwich)
+else:
+    print("you don't get a The Sandwich")
